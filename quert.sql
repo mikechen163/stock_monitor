2d306411bcd86f83d3df8ea858a5307ff9b7d6d3
mike chen <mikechen163@hotmail.com>
2015-07-12 11:26:45 +0800

    add sync.rb

--- quert.sql
+++ quert.sql
@@ -4,4 +4,7 @@ select * from lastest_records_1m where code= '399905' order by id desc limit 20;
 
 
 select (time) from lastest_records where code= '399905' order by id desc limit 20;
-select (time) from lastest_records_60m where code= '399905' order by id desc limit 20;
+select (time) from lastest_records_5m where code= '399905' order by id desc limit 20;
+select (date) from lastest_records_5m where code= '399905' order by id desc limit 20;
+
+ruby sync.rb -c /Users/mike/usbbackup "/Volumes/My Passport/photo"
